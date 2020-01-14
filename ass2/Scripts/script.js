40ea28f31dcdc9031dc8b4f09b2759dbac1f72bf
EndPositive <jop-zitman@hotmail.com>
2020-01-14 12:22:32 +0100

    Sorttable library + db connection

--- ass2/Scripts/script.js
+++ ass2/Scripts/script.js
@@ -1,74 +1,41 @@
-function getCell(row, col) {
-    let content = row.getElementsByTagName("td")[col].textContent.toLowerCase();
 
-    let date = Date.parse(content);
-    if (!isNaN(date)) {
-        return date;
-    }
+$(document).ready(function () {
+    $('table').tablesort();
 
-    let int = parseInt(content);
-    if (!isNaN(int)) {
-        return parseInt(content);
-    }
-    
-    return content;
-}
-// Insertion sort O(n^2)
-function sortTable(table, col = 0) {
-    let rows, tbody, len, type, oneval, iminval, ival, jminval, jval;
-    rows = table.rows;
-    tbody = table.getElementsByTagName("tbody")[0];
-    len = tbody.children.length;
-    for (let i = 2; i <= len; i++) {
-        oneval = getCell(rows[1], col);
-        iminval = getCell(rows[i-1], col);
-        ival = getCell(rows[i], col);
-        if (ival < oneval) {
-            tbody.insertBefore(rows[i], rows[1]);
-        } else if (ival < iminval) {
-            for (let j = 2; j < i; j++) {
-                jminval = getCell(rows[j-1], col);
-                jval = getCell(rows[j], col);
-                if (ival > jminval && ival < jval) {
-                    tbody.insertBefore(rows[i], rows[j]);
-                }
-            }
-        }
-        
-    }
-}
+    $('#reset').click(resetTable);
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    let headers, nodes, table, col;
-    headers = document.getElementsByTagName("th");
-    for (let i = 0; i < headers.length; i++) {
-        headers[i].onclick = function () {
-            table = headers[i].parentElement.parentElement.parentElement;
-            col = Array.prototype.indexOf.call(headers[i].parentNode.children, headers[i])
-            sortTable(table, col);
-        };
-    }
-    
-    var reset = document.getElementById("reset");
-    reset.addEventListener("click", resetTable);  
-    
-    var submit = document.getElementById("submit");
-    submit.addEventListener("click", putTable);
-    
-    var refresh = document.getElementById("refresh");
-    refresh.addEventListener("click", refreshTable);
-    
-    document.getElementById("top_selling_items_form").onsubmit = function() {
-        return false;
-    };
-    
+    $('#refresh').click(refreshTable);
+
+    refreshTable();
 });
+
+function putTable() {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", refreshTable);
+    xhr.responseType = "json";
+    var brand = document.getElementsByName("brand")[0];
+    var model = document.getElementsByName("model")[0];
+    var os = document.getElementsByName("os")[0];
+    var image = document.getElementsByName("image")[0];
+    var screensize = document.getElementsByName("screensize")[0];
+
+    var queryString = "brand=" + brand.value +
+        "&model=" + model.value +
+        "&os=" + os.value +
+        "&image=" + image.value +
+        "&screensize=" + screensize.value;
+    xhr.open("POST", "https://wt.ops.labs.vu.nl/api20/412d87b0");
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+    xhr.send(queryString);
+    return false;
+}
 
 function resetTable() {
     var xhr = new XMLHttpRequest();
     xhr.addEventListener("load", refreshTable);
     xhr.open("GET", "https://wt.ops.labs.vu.nl/api20/412d87b0/reset");
     xhr.send();
+    return;
 }
 
 function refreshTable() {
@@ -77,48 +44,31 @@ function refreshTable() {
     xhr.responseType = "json";        
     xhr.open("GET", "https://wt.ops.labs.vu.nl/api20/412d87b0");
     xhr.send();
-}
-
-function putTable() {
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener("load", refreshTable);
-    xhr.responseType = "json";        
-    var brand = document.getElementsByName("brand")[0];
-    var model = document.getElementsByName("model")[0];
-    var os = document.getElementsByName("os")[0];
-    var image = document.getElementsByName("image")[0];
-    var screensize = document.getElementsByName("screensize")[0];
-
-    var queryString = "brand=" + encodeURIComponent(brand.value) + 
-        "&model=" + encodeURIComponent(model.value) + 
-        "&os=" + encodeURIComponent(os.value) + 
-        "&image=" + encodeURIComponent(image.value) + 
-        "&screensize=" + encodeURIComponent(screensize.value);
-    xhr.open("POST", "https://wt.ops.labs.vu.nl/api20/412d87b0?" + queryString);
-    console.log("https://wt.ops.labs.vu.nl/api20/412d87b0?" + queryString);
-    xhr.send();
+    return;
 }
 
 function updateTable() {
     var topSellingItemTable = document.getElementById("top_selling_items_table").getElementsByTagName("tbody")[0];
     if (this.status === 200) {
         var items = this.response;
-        topSellingItemTable.innerHTML = "<tr>";
-        for (var i = 0; i < items.length; i++) {
-            console.log(items[i]);
-            var responsea = "<td>" + items[i].brand + "</td>" +
-                "<td>" + items[i].model + "</td>" +
-                "<td>" + items[i].os + "</td>" +
-                "<td>" + "<img src='" + items[i].image + "'></td>" +
-                "<td>" + items[i].screensize + "</td>";
-            
-            topSellingItemTable.innerHTML = topSellingItemTable.innerHTML + responsea;
-        }
-            topSellingItemTable.innerHTML = topSellingItemTable.innerHTML + "</tr>";
-
+        fillTable(items, topSellingItemTable);
     } else {
       movieInfo.innerHTML = "Error";
     }
+    return;
 }
 
+function fillTable(items, tbody) {
+    tbody.innerHTML = "<tr>";
+    for (var i = 0; i < items.length; i++) {
+        var responsea = "<td>" + items[i].brand + "</td>" +
+            "<td>" + items[i].model + "</td>" +
+            "<td>" + items[i].os + "</td>" +
+            "<td>" + "<img src='" + items[i].image + "'></td>" +
+            "<td data-sort-value='" + items[i].screensize + "'>" + items[i].screensize + "</td>";
 
+        tbody.innerHTML = tbody.innerHTML + responsea;
+    }
+    tbody.innerHTML = tbody.innerHTML + "</tr>";
+    return;
+}
