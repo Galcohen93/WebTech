cfdd69eff425617b6b3c030aea463f9a9a0f2523
EndPositive <jop-zitman@hotmail.com>
2020-01-21 18:46:44 +0100

    Refractor database.js and add some comments

--- ass3/database.js
+++ ass3/database.js
@@ -1,33 +1,25 @@
+var sqlite3 = require('sqlite3').verbose();
+const DBSOURCE = "db.sqlite";
 
-var sqlite3 = require('sqlite3').verbose()
-const DBSOURCE = "db.sqlite"
-
+// Initialize DB
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
-      // Cannot open database
-      console.error(err.message)
-      throw err
-    }else{
+        // Can't open DB
+        console.error(err.message);
+        throw err
+    } else {
+        // DB opened
         console.log('Connected to the SQLite database.')
-        db.run(`CREATE TABLE phones (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            brand text,
-            model text,
-            os text,
-            image text,
-            screensize int
-            )`,
-        (err) => {
-            if (err) {
-                // Table already created
-            }else{
-                // Table just created, creating some rows
-                var insert = 'INSERT INTO phones (brand, model,os,image,screensize) VALUES (?,?,?,?,?)'
-                db.run(insert, ["Apple","iPhone X","iOS","https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/IPhone_X_vector.svg/440px-IPhone_X_vector.svg.png","5"])
-                db.run(insert, ["Samsung","Galaxy s8","Android","https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Samsung_Galaxy_S8_and_S8_Plus.png/569px-Samsung_Galaxy_S8_and_S8_Plus.png","6"])
+        db.run("CREATE TABLE phones (id INTEGER PRIMARY KEY AUTOINCREMENT, brand text, model text, os text, image text, screensize int)", function (err) {
+            if (!err) {
+                // Table created
+                // Add placeholder rows
+                var sql = 'INSERT INTO phones (brand, model,os,image,screensize) VALUES (?,?,?,?,?)';
+                db.run(sql, ["Apple","iPhone X","iOS","https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/IPhone_X_vector.svg/440px-IPhone_X_vector.svg.png","5"])
+                db.run(sql, ["Samsung","Galaxy s8","Android","https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Samsung_Galaxy_S8_and_S8_Plus.png/569px-Samsung_Galaxy_S8_and_S8_Plus.png","6"])
             }
         });  
     }
 });
 
-module.exports = db
+module.exports = db;
