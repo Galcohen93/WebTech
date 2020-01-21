8b9d808b29ac3962d3c475ca7b86782cee0691d5
EndPositive <jop-zitman@hotmail.com>
2020-01-21 15:57:27 +0100

    Add path method to bonus. Add http request file to ass3.

--- bonus/main.go
+++ bonus/main.go
@@ -9,36 +9,57 @@ import (
 )
 var db *sql.DB
 var add *sql.Stmt
+var drop *sql.Stmt
+var patch *sql.Stmt
 var err error
 var rows *sql.Rows
 
 type Phone struct {
-	Id int `json: "id"`
-	Brand string `json: "brand"`
-	Model string `json: "model"`
-	Os string `json: "os"`
-	Image string `json: "image"`
-	Screensize int `json: "screensize"`
+	Id int `json:"id"`
+	Brand string `json:"brand"`
+	Model string `json:"model"`
+	Os string `json:"os"`
+	Image string `json:"image"`
+	Screensize int `json:"screensize"`
 }
-type Res struct {
-	Message string `json: "message"`
-	Data Phone `json: "data"`
-	Id int `json: "id"`
+
+func main(){
+	prepareDB()
+
+	createTable()
+
+	http.Handle("/", http.FileServer(http.Dir("www/")))
+
+	http.HandleFunc("/api/phones", phones)
+	http.HandleFunc("/api/reset", reset)
+
+	http.ListenAndServe(":8080", nil)
 }
-// DB STUFF
-func initDB() {
+
+func prepareDB() {
 	db, err = sql.Open("sqlite3", "tables.db")
 	if err != nil { panic(err) }
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS phones (id INTEGER PRIMARY KEY AUTOINCREMENT, brand text, model text, os text, image text, screensize int)")
+	drop, err = db.Prepare("DROP TABLE phones")
 	if err != nil { panic(err) }
 
 	add, err = db.Prepare("INSERT INTO phones (brand, model, os, image, screensize) VALUES (?, ?, ?, ?, ?)")
 	if err != nil { panic(err) }
-}
-func dropDB() {
-	_, err := db.Exec("DROP TABLE phones")
+
+	patch, err = db.Prepare("UPDATE phones set brand = COALESCE(?,brand),model = COALESCE(?,model),os = COALESCE(?,os),image = COALESCE(?,image),screensize = COALESCE(?,screensize) WHERE id = ?")
 	if err != nil { panic(err) }
+}
+
+func createTable() {
+	_, err = db.Exec("CREATE TABLE phones (id INTEGER PRIMARY KEY AUTOINCREMENT, brand text, model text, os text, image text, screensize int)")
+	if err == nil {
+		// If there is no error (DB didn't exist yet)
+		_, err = add.Exec("Apple", "iPhone X", "iOS", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/IPhone_X_vector.svg/440px-IPhone_X_vector.svg.png", "5")
+		if err != nil { panic(err) }
+
+		_, err = add.Exec("Samsung", "Galaxy s8", "Android", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Samsung_Galaxy_S8_and_S8_Plus.png/569px-Samsung_Galaxy_S8_and_S8_Plus.png", 6)
+		if err != nil { panic(err) }
+	}
 }
 
 func scanRowsToPhones(rows *sql.Rows) []*Phone {
@@ -54,6 +75,7 @@ func scanRowsToPhones(rows *sql.Rows) []*Phone {
 	if err := rows.Err(); err != nil { panic(err) }
 	return phones
 }
+
 func writeToJson(w http.ResponseWriter, res interface{}) {
 	jsonData, err := json.MarshalIndent(res, "", "    ")
 	if err != nil { panic(err) }
@@ -63,6 +85,8 @@ func writeToJson(w http.ResponseWriter, res interface{}) {
 
 // API Methods
 func phones(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 	switch r.Method {
 	case http.MethodGet:
 		var id = r.URL.Query().Get("id")
@@ -99,17 +123,37 @@ func phones(w http.ResponseWriter, r *http.Request){
 		if err != nil { panic(err) }
 		phone.Id = int(id)
 
-		var res Res
-		res.Message = "success"
-		res.Data = phone
-		res.Id = phone.Id
-
-		writeToJson(w, res)
+		writeToJson(w, phone)
 	case http.MethodDelete:
 		var id = r.URL.Query().Get("id")
 		if len(id) != 0 {
 			_, err = db.Exec(`DELETE FROM phones where id=?`, id)
 			if err != nil { panic(err) }
+
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	case http.MethodPatch:
+		var id = r.URL.Query().Get("id")
+
+		r.ParseForm()
+
+		var phone Phone
+		phone.Brand = r.Form.Get("brand")
+		phone.Model = r.Form.Get("model")
+		phone.Os = r.Form.Get("os")
+		phone.Image = r.Form.Get("image")
+		phone.Screensize, err = strconv.Atoi(r.Form.Get("screensize"))
+		if err != nil { panic(err) }
+		phone.Id, err = strconv.Atoi(id)
+		if err != nil { panic(err) }
+
+		if len(id) != 0 {
+			_, err = patch.Exec(phone.Brand, phone.Model, phone.Os, phone.Image, phone.Screensize, phone.Id)
+			if err != nil { panic(err) }
+
+			writeToJson(w, phone)
 		} else {
 			w.WriteHeader(http.StatusBadRequest)
 			return
@@ -120,29 +164,17 @@ func phones(w http.ResponseWriter, r *http.Request){
 }
 
 func reset(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 	switch r.Method {
-	case http.MethodGet:
-		dropDB()
-		initDB()
-		_, err = add.Exec("Apple", "iPhone X", "iOS", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/IPhone_X_vector.svg/440px-IPhone_X_vector.svg.png", "5")
+	case http.MethodDelete:
+		// Drop table
+		_, err = drop.Exec()
 		if err != nil { panic(err) }
 
-		_, err = add.Exec("Samsung", "Galaxy s8", "Android", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Samsung_Galaxy_S8_and_S8_Plus.png/569px-Samsung_Galaxy_S8_and_S8_Plus.png", 6)
-		if err != nil { panic(err) }
+		// Create table
+		createTable()
 	default:
 		w.WriteHeader(http.StatusNotFound)
 	}
-}
-
-func main(){
-	initDB()
-
-	http.Handle("/", http.FileServer(http.Dir("www/")))
-
-	http.HandleFunc("/api/phones", phones)
-	http.HandleFunc("/api/reset", reset)
-
-	// dropDB()
-
-	http.ListenAndServe(":8080", nil)
 }
